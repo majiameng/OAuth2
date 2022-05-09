a0a7f7d17ea435fbad4c275c9af7c7b50cf5d71d
tinymeng <666@majiamengcom>
2022-05-09 11:01:32 +0800

    解决bug：获取用户信息post方式，导致code获取不到

--- src/Gateways/Wechat.php
+++ src/Gateways/Wechat.php
@@ -196,7 +196,7 @@ class Wechat extends Gateway
             'appid'      => $this->config['app_id'],
             'secret'     => $this->config['app_secret'],
             'grant_type' => $this->config['grant_type'],
-            'code'       => isset($_GET['code']) ? $_GET['code'] : '',
+            'code'       => isset($_REQUEST['code']) ? $_REQUEST['code'] : '',
         ];
         return $params;
     }
